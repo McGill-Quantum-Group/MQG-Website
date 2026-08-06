5f49ac69975562884ee5688d47825cafc41a42bd
wennnu <wenyu007@hotmail.com>
2026-08-06 17:06:07 -0400

    added Fall Fest Sticker Large.png

--- src/components/Title.js
+++ src/components/Title.js
@@ -10,41 +10,8 @@ function Title(props) {
   }, []);
 
   return (
-    <div className="textOverlay">
-      <div className={`${startFade ? "text animate" : "text"}`}>
-        <p>
-          <b>Hello!</b>
-        </p>
-        <p style={{ fontSize: "34px", fontWeight: "325" }}>
-          We are the McGill Quantum Group.
-          <br />
-          Join our{" "}
-          <a
-            style={{
-              color: "#ff5d5d",
-              textDecoration: "none",
-            }}
-            href="https://discord.gg/KY4e9BrQNe"
-          >
-            Discord
-          </a>{" "}
-          to find the quickest updates.
-        </p>
-      </div>
-      <a
-        className={`${startFade ? "animate" : ""}`}
-        style={{
-          color: "white",
-          textAlign: "center",
-          width: "100%",
-          padding: "16px 0 8px 0",
-          textDecoration: "none",
-          marginTop: "auto",
-        }}
-        href="https://github.com/McGill-Quantum-Group/MQG-Website"
-      >
-        Made with ❤️ using React
-      </a>
+    <div className="mainDashboard">
+      <img src="../components/images/Fall Fest Sticker Large.png", alt="QFF 2026 Official Event">
     </div>
   );
 }

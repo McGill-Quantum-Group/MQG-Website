4783c78c521ef26e3ffe90126669ffaa46b203bc
wennnu <wenyu007@hotmail.com>
2026-08-06 22:00:36 -0400

    modifed Title.js

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
 import "./Title.css";
+import qffSticker from "../components/images/Fall Fest Sticker Large.png"
 
 function Title(props) {
   const [startFade, setStartFade] = useState(false);
@@ -9,9 +10,21 @@ function Title(props) {
     setStartFade(true);
   }, []);
 
+  const qffStickerStyle={
+    width:"80%",
+    height:"60%",
+  };
+
   return (
-    <div className="mainDashboard">
-      <img src="../components/images/Fall Fest Sticker Large.png", alt="QFF 2026 Official Event">
+    <div class="dashboardImage">
+      <img src={qffSticker} alt="Qiskit Fall Fest 2026" style={qffStickerStyle}/>
+    </div>
+    <div class="dashboardBody">
+      <h1>QISKIT FALL FEST 2026 IS BACK!</h1>
+      <p>
+        The McGill Quantum Group is hosting another Qiskit Fall Fest this year at the McGill Campus!
+        This year, we are celebrating a decade of quantum computing on the cloud. 
+      </p>
     </div>
   );
 }
